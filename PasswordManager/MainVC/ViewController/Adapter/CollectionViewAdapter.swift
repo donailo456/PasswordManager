74040b4937aa6173845b6c8c8014cf443e821a62
Danil Komarov <danil.komarov@vkteam.ru>
2025-04-07 13:53:39 +0300

    fixed bug

--- PasswordManager/MainVC/ViewController/Adapter/CollectionViewAdapter.swift
+++ PasswordManager/MainVC/ViewController/Adapter/CollectionViewAdapter.swift
@@ -71,9 +71,7 @@ private extension CollectionViewAdapter {
 
 extension CollectionViewAdapter: UICollectionViewDelegate {
     
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        <#code#>
-    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {}
     
 }
 
